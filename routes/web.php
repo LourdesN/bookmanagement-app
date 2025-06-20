958dfa51a3dbf90b537f5b41e30deafb91e971d5
Lourdes Nganga <lourdeswairimu@gmail.com>
2025-06-20 16:44:52 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show')->middleware('auth');
+
 
     // Profile password update
     Route::post('/profile/update-password', [ProfileController::class, 'updatePassword'])->name('profile.updatePassword');
