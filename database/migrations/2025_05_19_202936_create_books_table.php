611fddeb5eea3f369de5eb8d987253204fae5a6a
Lourdes Nganga <lourdeswairimu@gmail.com>
2025-06-20 11:46:44 +0300

    book migration

---
+++ database/migrations/2025_05_19_202936_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+     Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 500);
+            $table->date('print_date');
+            $table->decimal('unit_cost', 10, 2); // Assuming price format
+            $table->string('isbn')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('reorder_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
